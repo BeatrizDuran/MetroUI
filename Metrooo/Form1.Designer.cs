29a188273897279934345cd523c0bd889a9b3cec
tic-rt16311016@uthermosillo.edu.mx <beatrizkrn@hotmail.com>
2017-11-02 19:23:50 -0700

    Agregar archivos de proyecto.

---
+++ Metrooo/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Metrooo
+{
+    partial class frmPrueba
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTile5 = new MetroFramework.Controls.MetroTile();
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.SuspendLayout();
+            // 
+            // metroTile5
+            // 
+            this.metroTile5.ActiveControl = null;
+            this.metroTile5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.metroTile5.Location = new System.Drawing.Point(392, 63);
+            this.metroTile5.Name = "metroTile5";
+            this.metroTile5.Size = new System.Drawing.Size(341, 302);
+            this.metroTile5.TabIndex = 5;
+            this.metroTile5.Text = "Outlock";
+            this.metroTile5.TileImage = global::Metrooo.Properties.Resources.if_Outlook_410510;
+            this.metroTile5.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile5.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile5.UseSelectable = true;
+            this.metroTile5.UseTileImage = true;
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.ActiveControl = null;
+            this.metroTile4.Location = new System.Drawing.Point(184, 217);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(155, 148);
+            this.metroTile4.TabIndex = 4;
+            this.metroTile4.Text = "Games";
+            this.metroTile4.TileImage = global::Metrooo.Properties.Resources.if_Games1_2316002;
+            this.metroTile4.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile4.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile4.UseSelectable = true;
+            this.metroTile4.UseTileImage = true;
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.ActiveControl = null;
+            this.metroTile3.Location = new System.Drawing.Point(23, 217);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(155, 148);
+            this.metroTile3.TabIndex = 3;
+            this.metroTile3.Text = "Chrome";
+            this.metroTile3.TileImage = global::Metrooo.Properties.Resources.if_Google_Chrome_1298719;
+            this.metroTile3.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile3.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile3.UseSelectable = true;
+            this.metroTile3.UseTileImage = true;
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.ActiveControl = null;
+            this.metroTile2.Location = new System.Drawing.Point(184, 63);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(155, 148);
+            this.metroTile2.TabIndex = 2;
+            this.metroTile2.Text = "People";
+            this.metroTile2.TileImage = global::Metrooo.Properties.Resources.if_Technology_Mix___Final_16_998664;
+            this.metroTile2.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile2.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile2.UseSelectable = true;
+            this.metroTile2.UseTileImage = true;
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(23, 63);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(155, 148);
+            this.metroTile1.TabIndex = 1;
+            this.metroTile1.Text = "Skype";
+            this.metroTile1.TileImage = global::Metrooo.Properties.Resources.if_Skype_345412;
+            this.metroTile1.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.UseTileImage = true;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // frmPrueba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 403);
+            this.Controls.Add(this.metroTile5);
+            this.Controls.Add(this.metroTile4);
+            this.Controls.Add(this.metroTile3);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroTile1);
+            this.Name = "frmPrueba";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroTile metroTile4;
+        private MetroFramework.Controls.MetroTile metroTile5;
+    }
+}
+
---
+++ Metrooo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metrooo
+{
+    public partial class frmPrueba : MetroFramework.Forms.MetroForm
+    {
+        public frmPrueba()
+        {
+            Thread thread = new Thread(new ThreadStart(Metodo));
+            thread.Start();
+            InitializeComponent();
+            string tiempo = string.Empty;
+            for (int i = 0; i < 1000000; i++)
+            {
+                tiempo += i.ToString();
+            }
+            thread.Abort();
+        }
+        Mutex mutex = new Mutex();
+        void Metodo()
+        {
+            mutex.WaitOne();
+            SplashScreen.SplashForm frm = new SplashScreen.SplashForm();
+            frm.Name = "Dashboard";
+            Application.Run(frm);
+            mutex.ReleaseMutex();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+            MetroFramework.MetroMessageBox.Show(this,"Hola bienvenido a Skype","Skype",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
